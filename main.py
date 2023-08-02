a10f873d170fc7442b8354867fb354f98d3c1cb3
Kabir Hossain <133724174+kabirua@users.noreply.github.com>
2023-08-01 17:20:18 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,107 @@
+# from model import *
+from preProcess import *
+import preProcess
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from keras.models import Model, load_model
+import cv2
+import numpy as np
+from PIL import ImageOps
+import matplotlib.pyplot as plt
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+
+save_to_dir=None
+
+data_gen_args = dict(rotation_range=0.2,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.05,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+myGene = preProcess.trainData(2,"/root_data/",'image','label2',data_gen_args,save_to_dir = save_to_dir)
+
+
+def unetModel(pretrained_weights=None, input_size=(512,512, 1)):
+    inputs = tf.keras.layers.Input(input_size)
+    conv1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    conv1 = tf.keras.layers.BatchNormalization()(conv1)
+
+    pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    conv2 = tf.keras.layers.BatchNormalization()(conv2)
+    pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    conv3 = tf.keras.layers.BatchNormalization()(conv3)
+    pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = tf.keras.layers.BatchNormalization()(conv4)
+    drop4 = tf.keras.layers.Dropout(0.5)(conv4)
+    pool4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    conv5 = tf.keras.layers.BatchNormalization()(conv5)
+
+    drop5 = tf.keras.layers.Dropout(0.5)(conv5)
+
+
+    up6 = tf.keras.layers.Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        tf.keras.layers.UpSampling2D(size=(2, 2))(drop5))
+    merge6 = tf.keras.layers.concatenate([drop4, up6], axis=3)
+    conv6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = tf.keras.layers.BatchNormalization()(conv6)
+    conv6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+    conv6 = tf.keras.layers.BatchNormalization()(conv6)
+
+    up7 = tf.keras.layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        tf.keras.layers.UpSampling2D(size=(2, 2))(conv6))
+    merge7 = tf.keras.layers.concatenate([conv3, up7], axis=3)
+    conv7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = tf.keras.layers.BatchNormalization()(conv7)
+    conv7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+    conv7 = tf.keras.layers.BatchNormalization()(conv7)
+
+    up8 = tf.keras.layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        tf.keras.layers.UpSampling2D(size=(2, 2))(conv7))
+    merge8 = tf.keras.layers.concatenate([conv2, up8], axis=3)
+    conv8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = tf.keras.layers.BatchNormalization()(conv8)
+    conv8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+    conv8 = tf.keras.layers.BatchNormalization()(conv8)
+
+    up9 = tf.keras.layers.Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+        tf.keras.layers.UpSampling2D(size=(2, 2))(conv8))
+    merge9 = tf.keras.layers.concatenate([conv1, up9], axis=3)
+    conv9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = tf.keras.layers.BatchNormalization()(conv9)
+    conv9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = tf.keras.layers.BatchNormalization()(conv9)
+    conv10 = tf.keras.layers.Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=conv10)
+    model.compile(optimizer=Adam(lr = 1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+model = unetModel()
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+# model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model.fit(myGene,steps_per_epoch=300,epochs=5,callbacks=[model_checkpoint])
+
+
+model.save('model_root_test.h5')
+testGene = test_data2.testData("/root_data2/test3/")
+
+# results = model.predict_generator(testGene,30,verbose=1)
+results = model.predict(testGene,5,verbose=1)
+plt.imshow(results[0],aspect="auto",cmap='gray')
